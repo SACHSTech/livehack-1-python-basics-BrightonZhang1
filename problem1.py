d6eceec9c6d8a3f7f36a24a48a2e80551d8210be
Brighton Zhang <brighton.zhang23@ycdsbk12.ca>
2020-12-07 18:34:56 +0000

    Completed Livehack 1

--- problem1.py
+++ problem1.py
@@ -1,1 +1,16 @@
+'''
+--------------------------
+Name: problem1.py
+Purpose: Calculates Celsius to Fahrenheit
 
+Author: Brighton Zhang
+
+Created: date 07/12/2020
+--------------------------
+'''
+
+#User inputted Celsius.
+celsius = float(input("What is the Temperature in Celsius?: "))
+
+#Celsius calculated into Fahrenheit.
+print("The temperature in fahrenheit is:", (celsius * 9)/5+32)
